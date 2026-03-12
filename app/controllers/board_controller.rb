14b9eb24be164d6ac56799fb01e2558d4f50e565
Artem Rybintsev <art.rybintsev@gmail.com>
2026-03-12 22:09:23 +0300

    Isolate game state per session

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,5 +1,22 @@
 class BoardController < ApplicationController
   def index
-    @game = Game.last || Game.create
+    @game = current_game
+  end
+
+  private
+
+  def current_game
+    if session[:game_id].present?
+      existing = Game.find_by(id: session[:game_id])
+      return existing if existing
+    end
+
+    create_new_game
+  end
+
+  def create_new_game
+    game = Game.create
+    session[:game_id] = game.id
+    game
   end
 end
